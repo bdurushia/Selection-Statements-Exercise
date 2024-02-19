1313f96d6a75df19329c4c2386fe256fbc9b17cc
Bryan Durushia <b.durushia@gmail.com>
2024-02-19 14:39:06 -0600

    Completed Exercise 1 (Random number guesser).

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace SelectionStatementExercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Exercise 1
+
+            Random randomNumber = new Random();
+            int favNumber = randomNumber.Next(1, 11);
+
+            Console.WriteLine("Guess my favorite number:");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < favNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (userGuess > favNumber) 
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            { 
+                Console.WriteLine("You guessed it!!!");
+            }
         }
     }
 }
